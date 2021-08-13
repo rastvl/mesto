c32b39eb50e1f8226f12b6f5ca542e234db80903
Vladislav Rastoropov <rastmath23@gmail.com>
2021-08-13 21:43:21 +0300

    mesto

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+!function () {
+    let formElement = document.querySelector('.popup__form');
+    let nameInput = formElement.querySelectorAll('.popup__input')[0];
+    let jobInput = formElement.querySelectorAll('.popup__input')[1];
+
+    // Обработчик «отправки» формы, хотя пока
+    // она никуда отправляться не будет
+    function formSubmitHandler(evt) {
+        evt.preventDefault();
+        let name = nameInput.value;
+        let job = jobInput.value;
+
+        let profileName = document.querySelector('.profile__name');
+        let profileDesc = document.querySelector('.profile__description');
+
+        profileName.textContent = name;
+        profileDesc.textContent = job;
+    }
+
+    formElement.addEventListener('submit', formSubmitHandler);
+
+    let editBtn = document.querySelector('.profile__edit-button');
+    editBtn.addEventListener('click', _ => {
+        let popup = document.querySelector('.popup'); 
+        popup.style.display = "block";
+        // popup.className = "popup popup_opened";
+    })
+
+    let closeBtn = document.querySelector('.popup__close');
+    closeBtn.addEventListener('click', _ => {
+        let popup = document.querySelector('.popup');
+        popup.style.display = "none";
+    })
+}()
+
