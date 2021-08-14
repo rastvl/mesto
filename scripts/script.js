52a411cf112f1970c455e7e4aa0856f8ad0ea074
Vladislav Rastoropov <rastmath23@gmail.com>
2021-08-14 20:47:36 +0300

    fixes after review

--- scripts/script.js
+++ scripts/script.js
@@ -3,33 +3,28 @@
     let nameInput = formElement.querySelectorAll('.popup__input')[0];
     let jobInput = formElement.querySelectorAll('.popup__input')[1];
     let popup = document.querySelector('.popup'); 
-    // Обработчик «отправки» формы, хотя пока
-    // она никуда отправляться не будет
+    let editBtn = document.querySelector('.profile__edit-button');
+    let profileName = document.querySelector('.profile__name');
+    let profileDesc = document.querySelector('.profile__description');
+
     function formSubmitHandler(evt) {
         evt.preventDefault();
-        let name = nameInput.value;
-        let job = jobInput.value;
-
-        let profileName = document.querySelector('.profile__name');
-        let profileDesc = document.querySelector('.profile__description');
-
-        profileName.textContent = name;
-        profileDesc.textContent = job;
-        popup.style.display = "none";
+        profileName.textContent = nameInput.value;
+        profileDesc.textContent = jobInput.value;
+        popup.className = "popup";
     }
 
     formElement.addEventListener('submit', formSubmitHandler);
 
-    let editBtn = document.querySelector('.profile__edit-button');
     editBtn.addEventListener('click', _ => {
-        popup.style.display = "block";
-        // popup.className = "popup popup_opened";
+        popup.className += " popup_opened";
+        nameInput.value = profileName.textContent;
+        jobInput.value = profileDesc.textContent;
     })
 
     let closeBtn = document.querySelector('.popup__close');
     closeBtn.addEventListener('click', _ => {
-        let popup = document.querySelector('.popup');
-        popup.style.display = "none";
+        popup.className = "popup";
     })
 }()
 
